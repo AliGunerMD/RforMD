93dcd239720971ad0fa5d9c8c6841b5e0c0500be
AliGunerMD <draliguner@yahoo.com>
2023-11-28 21:10:29 +0300

    [color palette update]

--- R/ag_colors.R
+++ R/ag_colors.R
@@ -8,35 +8,91 @@
 #' # Example usage
 #' to_select_colors("blue")
 #' }
-#' @export
+
 
 to_select_colors <- function(...) {
 
         selected_colors <- c(
-                "green" = "#006400",
-                "light green" = "#90ee90",
-                "teal green" = "#00827f",
-                "blue" = "#6BAED6",
-                "turquoise" = "#40e0d0",
-                "light blue" = "#9ECAE1",
-                "dark blue" = "#4292C6",
-                "dodger blue" = "#1e90ff",
-                "ferrari" = "#ff2800",
-                "light red" = "#FC9272",
-                "dark red" = "#8b0000",
-                "orange" = "#ff8c00",
-                "ligth orange" = "#FFC073",
-                "coral red" = "#ff4040",
-                "gray" = "#808080",
-                "light grey" = "#d3d3d3",
-                "dark gray" = "#555555",
-                # "beige" = "#f5f5dc",
-                # "milk" = "#fdfff5",
-                # "white" = "#ffffff",
-                "black" = "#000000",
-                "TS red" = "#a41d34",
-                "TS blue" = "#14c0f1"
+                        "Lime" = "#00FF00",
+                        "Forest Green" = "#228B22",
+                        "Dark Green" = "#006400",
+                        "Spring Green" = "#00FF7F",
+                        "Sea Green" = "#2E8B57",
+                        "Amazon" = "#3b7a57",
+                        "Mint Green" = "#98FF98",
+                        "Pistachio" = "#93C572",
+                        "Hunter Green" = "#355E3B",
+                        "Grass Green" = "#7CFC00",
+                        "Dollar Bill" = "#85BB65",
+                        "Camouflage Green" = "#78866B",
+                        "Pear" = "#D1E231",
+                        "Heineken Green" = "#008200",
+                        "Pine Green" = "#01796F",
+                        "Dark Olive Green" = "#556B2F",
+                        "Light Sky Blue" = "#87CEFA",
+                        "Dodger Blue" = "#1E90FF",
+                        "Dark Blue" = "#00008B",
+                        "Sky Blue" = "#87CEEB",
+                        "Azure" = "#007FFF",
+                        "Sapphire" = "#0F52BA",
+                        "Deep Sky Blue" = "#00BFFF",
+                        "Indigo" = "#3F00FF",
+                        "Navy" = "#000080",
+                        "Vivid Sky Blue" = "#00CCFF",
+                        "Facebook" = "#1877F2",
+                        "IBM" = "#0530AD",
+                        "Twitter" = "#1DA1F2",
+                        "Argentinan" = "#6CB4EE",
+                        "Turquoise" = "#40E0D0",
+                        "Youtube" = "#FF0000",
+                        "Liverpool" = "#C8102E",
+                        "Maroon" = "#800000",
+                        "Tomato" = "#FF6347",
+                        "Crimson" = "#DC143C",
+                        "Carmine" = "#960018",
+                        "Ferrari" = "#FF2800",
+                        "Cadmium Red" = "#E30022",
+                        "Dark Scarlet" = "#560319",
+                        "Dark Orange" = "#FF8C00",
+                        "Pumpkin" = "#FF7518",
+                        "Burnt Orange" = "#CC5500",
+                        "Orange" = "#FFA500",
+                        "Persimmon" = "#EC5800",
+                        "Sinopia" = "#CB410B",
+                        "Peru" = "#CD853F",
+                        "Chocolate" = "#D2691E",
+                        "Saddle Brown" = "#8B4513",
+                        "Deer" = "#BA8759",
+                        "Bronze" = "#CD7F32",
+                        "Sepia" = "#704214",
+                        "Orchid" = "#DA70D6",
+                        "Dark Orchid" = "#9932CC",
+                        "Deep Fuchsia" = "#C154C1",
+                        "Mardi Gras" = "#880085",
+                        "Dark Magenta" = "#8B008B",
+                        "Gold" = "#FFD700",
+                        "Mustard" = "#FFDB58",
+                        "Sunglow" = "#FFCC33",
+                        "Lemon Yellow" = "#FFF44F",
+                        "Metallic Sunburst" = "#9c7c38",
+                        "Silver" = "#C0C0C0",
+                        "Onyx" = "#353839",
+                        "Charcoal" = "#36454F",
+                        "Smoky Black" = "#100C08",
+                        "Vampire Black" = "#080808",
+                        "Dark charcoal" = "#333333",
+                        "Dark Slate Grey" = "#2f4f4f",
+                        "Dark Midnight Blue" = "#000036",
+                        "Snow" = "#FFFAFA",
+                        "Ivory" = "#FFFFF0",
+                        "Milk" = "#FDFFF5",
+                        "Black" = "#000000",
+                        "TS red" = "#a41d34",
+                        "TS blue" = "#14c0f1"
                 )
+
+
+
         cols <- c(...)
 
         if (is.null(cols))
@@ -56,29 +112,29 @@ to_select_colors <- function(...) {
 #' colors
 #' show_col(karadeniz("Vazil")(12))
 #'}
-#'@export
 #'
 #'
 karadeniz <- function(palette = "main", reverse = FALSE, ...) {
   mypalettes <- list(
-    "Hidirnebi" = to_select_colors("ferrari", "dodger blue", "gray", "green", "orange"), # to_select_colors(sample(names(selected_colors), 6)),
-    "Kadirga" = to_select_colors("turquoise", "ferrari", "dodger blue", "coral red"),
-    "Kayabasi" = to_select_colors("dark blue", "green", "ferrari", "gray"),
-    "Cal" = to_select_colors("teal green", "orange", "blue", "turquoise", "ferrari"),
-    "Pokut" = to_select_colors("green", "gray", "dark blue", "ligth orange", "black"),
-    "Samistal" = to_select_colors("coral red", "green", "dark red"),
-    "Elevit" = to_select_colors("ferrari", "teal green", "dodger blue", "black"),
-    "Gito" = to_select_colors("dark red", "dark blue", "light grey", "teal green"),
-    "Ovit" = to_select_colors("dodger blue", "light blue", "light blue", "light grey", "teal green"),
-    "Palovit" = to_select_colors("light blue", "dark red", "teal green", "orange"),
-    "Faroz" = to_select_colors("TS red", "TS blue"),
-    "Vazil" = to_select_colors("green", "turquoise", "ferrari", "light blue")
+          "Hidirnebi" = to_select_colors("Dark Blue", "Pistachio", "Amazon", "Charcoal", "Smoky Black"),
+          "Kadirga" = to_select_colors("Deep Sky Blue", "Argentinan", "Dodger Blue", "Deep Fuchsia", "Dark Orchid", "Dark charcoal"),
+          "Kayabasi" = to_select_colors("Forest Green",  "Dark Olive Green", "Saddle Brown", "Dark Slate Grey"),
+          "Cal" = to_select_colors("Orange", "Pumpkin",  "Crimson", "Chocolate", "Dark Scarlet"),
+          "Pokut" = to_select_colors("Twitter",  "Facebook", "IBM", "Dark Midnight Blue"),
+          "Samistal" = to_select_colors( "Vivid Sky Blue", "Sapphire", "Liverpool", "Burnt Orange", "Vampire Black"),
+          "Elevit" = to_select_colors("Chocolate", "Saddle Brown", "Peru", "Cadmium Red"),
+          "Gito" = to_select_colors("Dark Olive Green", "Sepia", "Persimmon", "Pumpkin"),
+          "Ovit" = to_select_colors("Metallic Sunburst", "Sunglow", "Gold", "Dark Orange", "Black"),
+          "Palovit" = to_select_colors("Tomato", "Liverpool", "Cadmium Red",  "Carmine","Maroon", "Smoky Black"),
+          "Faroz" = to_select_colors("TS red", "TS blue"),
+          "Vazil" = to_select_colors("Lime", "Camouflage Green", "Gold", "Dark charcoal")
+
   )
   pal <- mypalettes[[palette]]
 
   if (reverse) pal <- rev(pal)
 
-  colorRampPalette(pal, bias = 2, ...)
+  colorRampPalette(pal, bias = .5, ...)
 }
 
 #' @title scale_color_karadeniz
