f6d7cc1d4d4292aca678c390e1d0d52d4b4efae7
AliGunerMD <draliguner@yahoo.com>
2023-11-29 03:24:48 +0300

    [ag_flex was modified]

--- R/flextable_functions.R
+++ R/flextable_functions.R
@@ -1,9 +1,38 @@
-#' @title Create a Styled Flextable
+#' @title Define Colors for Table Styling
+#' @description
+#' This code chunk defines colors for different elements of a table, such as the table header, footer,
+#' border, background of the table header, and the overall table border.
 #'
+#' @author Ali Guner
+#'
+#' @return A set of color values for table styling.
+#'
+#' @examples
+#' \dontrun{
+#' # Assuming you want to use these colors in table styling
+#' iris %>%
+#' ag_flex() %>%
+#' flextable::bg(i = 1, j = NULL, bg = col_table_header, part = "header")
+#' }
+#'
+#' @importFrom officer fp_border
+
+col_table_header <- ag_colors("blues")[4]
+col_footer <- ag_colors("grays")[6]
+col_table_border <- ag_colors("grays")[5]
+table_border <- officer::fp_border(color = col_table_border)
+col_table_header_bg <- ag_colors("grays")[2]
+
+
+
+
+
+
+#' @title Create a Styled Flextable
 #' @description
 #' This function generates a styled flextable with specified font family and font size.
-#' The flextable is configured with default font settings, the 'theme_booktabs', and additional styling to make the first row bold.
-#'
+#' The flextable is configured with default font settings, the 'theme_booktabs', and additional styling
+#' to make the first row bold in the header.
 #' @param data A data frame or a tibble to be displayed in the flextable.
 #' @param flex_font_family Character, font family for the text in the flextable.
 #' @param flex_font_size Numeric, font size for the text in the flextable.
@@ -23,10 +52,14 @@
 #' }
 #'
 #' @importFrom flextable flextable set_flextable_defaults theme_booktabs bold
+#' @importFrom officer fp_border
 
 
 
-ag_flex <- function(data, flex_font_family = "Arial", flex_font_size = 14) {
+
+
+
+ag_flex <- function(data, flex_font_family = "Arial", flex_font_size = 11) {
 
         flextable::set_flextable_defaults(
                 font.size = flex_font_size,
@@ -51,9 +84,9 @@ ag_flex <- function(data, flex_font_family = "Arial", flex_font_size = 14) {
 
 
 
-
 #' Create Landscape Page Section for Flextable
 #'
+#' @description
 #' This function generates a landscape-oriented page section for use in creating a Microsoft Word document with the `officer` package.
 #' The section is defined with landscape orientation, and default page margins are applied.
 #'
@@ -74,14 +107,19 @@ ag_flex <- function(data, flex_font_family = "Arial", flex_font_size = 14) {
 #' }
 #'
 #' @importFrom officer page_size page_mar prop_section
+
+
 flex_landscape <- officer::prop_section(
         page_size = officer::page_size(orient = "landscape"),
         type = "nextPage",
         page_margins = officer::page_mar()
 )
 
+
+
 #' Create Portrait Page Section for Flextable
 #'
+#' @description
 #' This function generates a portrait-oriented page section for use in creating a Microsoft Word document with the `officer` package.
 #' The section is defined with portrait orientation and a specified width, and default page margins are applied.
 #'
@@ -102,6 +140,8 @@ flex_landscape <- officer::prop_section(
 #' }
 #' @importFrom officer page_size page_mar prop_section
 #'
+
+
 flex_portrait <- officer::prop_section(
         page_size = officer::page_size(orient = "portrait", width = 14),
         type = "nextPage",
