8be7bd48ec14d7d4799c3933c162a44f4c4efae8
AliGunerMD <draliguner@yahoo.com>
2023-12-15 01:02:21 +0300

    ag_ff_summary exclude strata

--- R/finalfit_functions.R
+++ R/finalfit_functions.R
@@ -503,7 +503,7 @@ ag_ff_summary <- function(.dataset, strata = NULL, table_vars,
 
           # This one looked more safe, gave up the previous YN_vars version
           .dataset <- .dataset %>%
-                  dplyr::mutate(dplyr::across(where(is.factor), ~ dplyr::if_else(tolower(.) == "yes", paste0(".", .), .)))
+                  dplyr::mutate(dplyr::across(where(is.factor) & !dplyr::cur_column() %in% .dataset[[strata]], ~ dplyr::if_else(tolower(.) == "yes", paste0(".", .), .)))
   }
 
 
